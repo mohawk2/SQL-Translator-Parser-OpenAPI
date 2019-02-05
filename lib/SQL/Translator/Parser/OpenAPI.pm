b86cf01965a7428b07f388d0186d91e94d28a7f1
Ed J <mohawk2@users.noreply.github.com>
2019-02-05 05:16:55 +0000

    v0.06

--- lib/SQL/Translator/Parser/OpenAPI.pm
+++ lib/SQL/Translator/Parser/OpenAPI.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use JSON::Validator::OpenAPI::Mojolicious;
 
-our $VERSION = "0.05";
+our $VERSION = "0.06";
 use constant DEBUG => $ENV{SQLTP_OPENAPI_DEBUG};
 use String::CamelCase qw(camelize decamelize);
 use Lingua::EN::Inflect::Number qw(to_PL to_S);
